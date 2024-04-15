031288529646884ecb6fd93ad07d18d318066919
Ankit Gandhi <agproxy21@yahoo.com>
2024-04-15 11:23:06 +0530

    Updated Solution - GfG to GitHub

--- Medium/Minimum number of jumps/minimum-number-of-jumps.cpp
+++ Medium/Minimum number of jumps/minimum-number-of-jumps.cpp
@@ -9,26 +9,29 @@ using namespace std;
 class Solution{
   public:
     int minJumps(int arr[], int n){
-        int maxi=0;
-       int maxrange=0;
-       int jump=0;
-       if(n==1){
-           return 0;
-       }
-       if(arr[0]==0){
-           return -1;
-       }
-       for(int i=0;i<n;i++){
-           maxi=max(maxi,i+arr[i]);
-           if(maxrange==i){
-               maxrange=maxi;
-               jump++;
-               if(maxrange>=n-1){
-                   return jump;
-               }
-           }
-       }
-       return -1;
+      if(n==1)return 0;
+        if(arr[0]==0)return -1;
+        int ans=0;
+        int i=0;
+        if(arr[0]>=n-1)return 1;
+        while(i<n){
+            int maxi=0;
+            int nextstep=0;
+            for(int j=1;j<=arr[i];j++){
+                if(arr[i+j]>=n-1-(i+j)){
+                    return ans+2;
+                }
+                if(arr[i+j]==0)continue;
+                if(arr[i+j]-(arr[i]-j)>maxi){
+                    maxi=arr[i+j]-(arr[i]-j);
+                    nextstep=j;
+                }
+            }
+            if(nextstep==0)return -1;
+            i+=nextstep;
+            ans++;
+    }
+    return ans;
     }
 };
 
