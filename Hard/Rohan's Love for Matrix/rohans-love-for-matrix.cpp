6dd8ba906a4c00db927cd9b246456a9b68371c5f
Ankit Gandhi <agproxy21@yahoo.com>
2024-04-23 19:33:19 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Rohan's Love for Matrix/rohans-love-for-matrix.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    int firstElement(int n) {
+        // code here
+        int a=1,b=1;
+        int tmp=a,mod=1e9+7;
+        for(int i=2;i<=n;i++){
+            tmp=b;
+            b=(b+a)%mod;
+            a=tmp;
+        }
+        return tmp;
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.firstElement(n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
