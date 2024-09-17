9dbd160160e75214256c6acc79e2de4b3b11cdcb
Ankit Gandhi <agproxy21@yahoo.com>
2024-09-18 02:30:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Left most and right most index/left-most-and-right-most-index.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    pair<long,long> indexes(vector<long long> v, long long x)
+    {
+        // code herelong
+        long long first=-1,second=-1;
+        
+        for(int i=0;i<v.size();i++){
+            if(v[i]==x){
+                first=i;
+                break;
+            }
+        }
+        for(int i=v.size()-1;i>=0;i--){
+            if(v[i]==x){
+                second=i;
+                break;
+            }
+        }
+        return {first, second};
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+ {
+    long long t;
+    cin>>t;
+    while(t--)
+    {
+        long long n, k;
+        vector<long long>v;
+        cin>>n;
+        for(long long i=0;i<n;i++)
+        {
+            cin>>k;
+            v.push_back(k);
+        }
+        long long x;
+        cin>>x;
+        Solution obj;
+        pair<long,long> pair = obj.indexes(v, x);
+        
+        if(pair.first==pair.second and pair.first==-1)
+        cout<< -1 <<endl;
+        else
+        cout<<pair.first<<" "<<pair.second<<endl;
+
+    }
+	return 0;
+}
+
+// } Driver Code Ends
